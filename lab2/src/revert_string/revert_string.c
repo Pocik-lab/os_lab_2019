376581fca6a51706c76034f7252aefa7fd393276
Pocik-lab <79202142+Pocik-lab@users.noreply.github.com>
2021-03-22 19:26:55 +0300

    Update revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -6,7 +6,7 @@ void RevertString(char *str)
 {
 
     char *c;
-    c = (char*)malloc(sizeof(10));
+    c = (char*)malloc(sizeof(str));
 	for (int i = 0, j = strlen(str) - 1; i < j; i++,  j--)
     {
         *c = str[i];
